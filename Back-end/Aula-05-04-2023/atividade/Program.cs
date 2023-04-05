57abdc27aa1c732eead087b1c82d4e3c004d196e
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2023-04-05 15:12:07 -0300

    Aula 05 04 2023 e atividade inserida

---
+++ Back-end/Aula-05-04-2023/atividade/Program.cs
@@ -0,0 +1,23 @@
+﻿// Inicialização das variáveis
+string nome;
+int idade;
+float salario;
+char estadoCivil;
+
+Console.WriteLine($"Insira as informações abaixo:");
+do
+{
+    Console.Write($"Nome: ");
+    nome = Console.ReadLine();
+    
+    Console.Write("Idade: ");
+    idade = int.Parse(Console.ReadLine());
+
+    Console.Write("Salário: ");
+    salario = float.Parse(Console.ReadLine());
+
+    Console.Write("Estado civil: ");
+    estadoCivil = char.Parse(Console.ReadLine().ToLower());
+    Console.WriteLine();
+}
+while (nome == "" || idade <= 0 || idade > 100 || salario <= 0 || estadoCivil != 's' && estadoCivil != 'c' && estadoCivil != 'v' && estadoCivil != 'd');
